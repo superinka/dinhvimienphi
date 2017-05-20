05ef9ebc9f6450b9b8c56cd1dedf17218ef755fe
superinka <thesuperinka@gmail.com>
2017-05-20 22:29:35 +0700

    new

--- wp/wp-content/themes/dinhvimienphi/templates/menu/main-menu.php
+++ wp/wp-content/themes/dinhvimienphi/templates/menu/main-menu.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo"></a>
+            <a class="navbar-brand" href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/top-logo.png" alt="logo"></a>
         </div>
 
         <div class="collapse navbar-collapse navbar-right">
